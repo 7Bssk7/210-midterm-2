3e0130a0e9aa2e2ca648c7f6683b4421ffe6a22a
7Bssk7 <arhip.fin3@gmail.com>
2025-10-19 19:28:04 -0700

    Update

--- 210-midterm-2-starter-1.cpp
+++ 210-midterm-2-starter-1.cpp
@@ -12,10 +12,10 @@ const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
 class DoublyLinkedList {
 private:
     struct Node {
-        int data;
+        string data;
         Node* prev;
         Node* next;
-        Node(int val, Node* p = nullptr, Node* n = nullptr) {
+        Node(string val, Node* p = nullptr, Node* n = nullptr) {
             data = val; 
             prev = p;
             next = n;
@@ -28,7 +28,7 @@ private:
 public:
     DoublyLinkedList() { head = nullptr; tail = nullptr; }
 
-    void insert_after(int value, int position) {
+    void insert_after(string value, int position) {
         if (position < 0) {
             cout << "Position must be >= 0." << endl;
             return;
@@ -59,7 +59,7 @@ public:
         temp->next = newNode;
     }
 
-    void delete_val(int value) {
+    void delete_val(string value) {
         if (!head) return;
 
         Node* temp = head;
@@ -119,7 +119,7 @@ public:
         delete temp;
     }
 
-    void push_back(int v) {
+    void push_back(string v) {
         Node* newNode = new Node(v);
         if (!tail)
             head = tail = newNode;
@@ -130,7 +130,7 @@ public:
         }
     }
     
-    void push_front(int v) {
+    void push_front(string v) {
         Node* newNode = new Node(v);
         if (!head)
             head = tail = newNode;
@@ -234,11 +234,14 @@ int main() {
     cout << "Store open:" << endl;
 
     for(int i = 0; i < MIN_LS; ++i){
-        string n = RandomName(names);
+        string n = randomName(names);
         list.push_back(n); 
         cout << n << "Joins the line" << endl;
     }
 
+    cout << "Resulting line: "<< endl;
+
+    list.print();
 
 
     
@@ -247,7 +250,7 @@ int main() {
     return 0;
 }
 
-string RandomName(const vector<string>& n){
 
-
+string randomName(const vector<string>& n){
+    return n.at(rand() % n.size());
 }
